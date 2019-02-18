f9845ac6295a6c9434cdea318926c5911d29e9b5
LMarsiske <47642996+LMarsiske@users.noreply.github.com>
2019-02-18 10:32:19 -0500

    Deleted logo.svg and created Person/Person.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		persons: [
+			{ name: "Max", age: 28 },
+			{ name: "Manu", age: 29 },
+			{ name: "Stephanie", age: 26 }
+		]
+	};
+
+	switchNameHandler = () => {
+		//console.log("Was Clicked!");
+		//Don't do this!!! this.state.persons[0].name = "Maximillian";
+		this.setState({
+			persons: [
+				{ name: "Max", age: 28 },
+				{ name: "Manu", age: 29 },
+				{ name: "Stephanie", age: 26 }
+			]
+		});
+	};
+
+	render() {
+		return (
+			<div className='App'>
+				<h1>Hi, I'm a React App!</h1>
+				<p>This is really working!</p>
+				<button onClick={this.switchNameHandler}>Switch Name</button>
+				<Person
+					name={this.state.persons[0].name}
+					age={this.state.persons[0].age}
+				/>
+				<Person
+					name={this.state.persons[1].name}
+					age={this.state.persons[1].age}
+				>
+					My Hobbies: Racing
+				</Person>
+				<Person
+					name={this.state.persons[2].name}
+					age={this.state.persons[2].age}
+				/>
+			</div>
+		);
+		/*return React.createElement(
+			"div",
+			{ className: "App" },
+			React.createElement("h1", null, "Does this work now?")
+		);*/
+	}
 }
 
 export default App;
